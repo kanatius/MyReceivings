bef59dc90732c506e2a6524c8bfadcd23285e690
kanatius <natanalmeidadelima@gmail.com>
2021-10-24 16:14:15 -0300

    spring boot project created

---
+++ backend-springboot/src/test/java/com/myreceivings/MyreceivingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myreceivings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyreceivingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
